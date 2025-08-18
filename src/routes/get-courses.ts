7f7af39227549df30e27ec307daa8438beefbed1
KaianDev <kaianvasconcelos@gmail.com>
2025-08-18 12:37:07 -0300

    feat: Enhance course retrieval with pagination and search
    Add query parameters for search, sorting, pagination, and limit
    to the course retrieval endpoint. This improvement allows users
    to filter and navigate through courses more effectively,
    enhancing the overall API usability and performance.

--- src/routes/get-courses.ts
+++ src/routes/get-courses.ts
@@ -1,3 +1,4 @@
+import { asc, ilike, or } from "drizzle-orm";
 import type { FastifyPluginCallbackZod } from "fastify-type-provider-zod";
 import z from "zod";
 import { db } from "../database/client.ts";
@@ -11,6 +12,12 @@ export const getCoursesRoute: FastifyPluginCallbackZod = (server) => {
 				tags: ["courses"],
 				summary: "List courses",
 				operationId: "get_courses",
+				querystring: z.object({
+					search: z.string().optional(),
+					sort: z.enum(["title", "createdAt"]).optional().default("createdAt"),
+					page: z.coerce.number().optional().default(1),
+					limit: z.coerce.number().optional().default(2),
+				}),
 				response: {
 					200: z.object({
 						courses: z.array(
@@ -19,19 +26,44 @@ export const getCoursesRoute: FastifyPluginCallbackZod = (server) => {
 								title: z.string(),
 							})
 						),
+						metadata: z.object({
+							total: z.number(),
+							pages: z.number(),
+							limit: z.number(),
+							currentPage: z.number(),
+						}),
 					}),
 				},
 			},
 		},
-		async () => {
-			const results = await db
-				.select({
-					id: coursesTable.id,
-					title: coursesTable.title,
-				})
-				.from(coursesTable);
+		async (request) => {
+			const { search, sort, page, limit } = request.query;
+			const condition = search
+				? or(
+						ilike(coursesTable.title, `%${search}%`),
+						ilike(coursesTable.description, `%${search}%`)
+					)
+				: undefined;
 
-			return { courses: results };
+			const [results, total] = await Promise.all([
+				db
+					.select({
+						id: coursesTable.id,
+						title: coursesTable.title,
+					})
+					.from(coursesTable)
+					.orderBy(asc(coursesTable[sort]))
+					.limit(limit)
+					.offset((page - 1) * limit)
+					.where(condition),
+				db.$count(coursesTable, condition),
+			]);
+			const pages = Math.ceil(total / limit);
+
+			return {
+				courses: results,
+				metadata: { total, pages, currentPage: page, limit },
+			};
 		}
 	);
 };
